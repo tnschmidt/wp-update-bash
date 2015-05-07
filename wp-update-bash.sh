d5cb9ea9b3a6c4ed0927696ee23b69b39f247f36
Thomas Schmidt <thomas.schmidt@uk2group.com>
2015-05-07 16:21:41 -0600

    Update wp-update-bash.sh

--- wp-update-bash.sh
+++ wp-update-bash.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-#title           	: update-wp.sh
-#description   		: This script will update wordpress version to latest
-#author		        : Thomas Schmidt
-#date          		: 20150507
-#version         	: 1    
-#usage		        :.\wp-update-bash.sh
-#notes           	: Should be placed in the user home directory next to the public_html
+#title          :update-wp.sh
+#description    :This script will update wordpress version to latest
+#author         :Thomas Schmidt
+#date           :20150507
+#version        :1
+#usage          :.\update-wp.sh
+#notes          : You may need to run chmod u+x update-wp-plugin.sh when creating. Should be placed in the user directory next to the public_html.
 #==============================================================================
 
 #some variables
-wp_admin_url='' #EXAMPLE http://www.your-wp-site.com/wp-login.php
-home_dir='' #EXAMPLE /home/your-user-home
-doc_root="$home_dir/public_html" #if your document root is not public html you will need to change this
+brand='westhost'
+wp_admin_url='http://westhost.com/blog/wp-login.php'
+home_dir='/home/4qc67hib'
+doc_root=”$home_dir/public_html”
 
 #get date for today to append to file names
 now=$(date +%Y%m%d)
@@ -22,7 +23,7 @@ cd $home_dir
 #backup the blog
 tar -czf $home_dir/blog-backup-$now.tar.gz $doc_root
 
-#backup db -- ENTER YOUR WP DB DETAILS HERE
+#backup db
 db_name=`cat $doc_root/wp-config.php | grep DB_NAME | cut -d \' -f 4`
 db_user=`cat $doc_root/wp-config.php | grep DB_USER | cut -d \' -f 4`
 db_pass=`cat $doc_root/wp-config.php | grep DB_PASSWORD | cut -d \' -f 4`
@@ -42,8 +43,8 @@ rm -rf $doc_root/wp-admin
 rm -rf $doc_root/wp-includes
 
 #replace wp-admin and wp-includes with what we just extracted
-mv $root_dir/wordpress/wp-admin $doc_root/wp-admin
-mv $root_dir/wordpress/wp-includes $doc_root/wp-includes
+mv $home_dir/wordpress/wp-admin $doc_root/wp-admin
+mv $home_dir/wordpress/wp-includes $doc_root/wp-includes
 
 #copy in the new wp-content base files
 /bin/cp $home_dir/wordpress/wp-content/* $doc_root/wp-content/
@@ -51,16 +52,16 @@ mv $root_dir/wordpress/wp-includes $doc_root/wp-includes
 /bin/cp $home_dir/wordpress/wp-content/plugins/* $doc_root/wp-content/plugins/
 
 #remove the wp-content directory in our new version as we dont want to move in the
-rm -r $root_dir/wordpress/wp-content
+rm -r $home_dir/wordpress/wp-content
 
 #lets move all the wordpress base files now into our doc root
-mv $root_dir/wordpress/* $doc_root/
+mv $home_dir/wordpress/* $doc_root/
 
 #now we can remove the wordpress directory that was holding our extracted new version of wordpress
-rm -r $root_dir/wordpress
+rm -r $home_dir/wordpress
 
 #remove latest.tar.gz file
-rm $root_dir/latest.tar.gz
+rm $home_dir/latest.tar.gz
 
 echo
 echo '************************************************'
@@ -69,5 +70,3 @@ echo 'IMPORTANT: Please log in to wp-admin at '$wp_admin_url' to see if a databa
 echo '************************************************'
 echo '************************************************'
 echo
-
-
